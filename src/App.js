180cf3346cfd0b0abcb533833890eee0a08696ed
shohruzjon-me <shohruzjon@vanalauri.com>
2022-11-18 20:07:56 +0300

    Proyekt Qo'shilmoqda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
+  
+  const [state, setState] = React.useState([]);
+  
+  async function getMonsters (){
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setState(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  React.useEffect(()=>{
+    getMonsters();
+  }, []);
+
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+           {
+            state.map((monster)=> <Card key={monster.id} monster={monster}/>)
+           }
     </div>
   );
 }
